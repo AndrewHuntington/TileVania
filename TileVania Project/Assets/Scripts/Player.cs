7aa812a14f1d748eaee0c2d83790a403885f47fc
strugglebunny <andrew.huntington@gmail.com>
2019-06-09 17:14:09 +0900

    Flip Character Sprite
    Add a method FlipSprite() to the Player script to controller wether the player is facing left or right while moving.

--- TileVania Project/Assets/Scripts/Player.cs
+++ TileVania Project/Assets/Scripts/Player.cs
@@ -18,7 +18,8 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         Run();
-	}
+        FlipSprite();
+    }
 
     // Controls movement along the X axis
     // Go to Edit > Project Settings > Input > Horizontal > Sensitivity to adjust input responsiveness
@@ -28,5 +29,20 @@ public class Player : MonoBehaviour {
         float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is between -1 and +1
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
+    }
+
+
+    // Checks which way the character is moving and flips the sprite accordingly
+    private void FlipSprite()
+    {
+        // First, check if the player is moving
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; // Mathf.Epsilon is a stand in for 0
+
+        // Mathf.Sign takes a number and returns a value of 1 if the value is positive or 0, -1 if the value is negative
+        if (playerHasHorizontalSpeed)
+        {
+            //transform.localScale = new Vector2(x,y)  -- allows you access to the X and Y values of Transform > Scale
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
     }
 }
