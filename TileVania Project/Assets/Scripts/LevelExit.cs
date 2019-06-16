21d8156caf519e960c3f3accf1a245bade8a27a6
strugglebunny <andrew.huntington@gmail.com>
2019-06-16 18:41:42 +0900

    Added Start Button + Exit Portal
    -Added a start button to the game.
    -Added a way to move between levels.

---
+++ TileVania Project/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour {
+
+    [SerializeField] float levelLoadDelay = 1f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(NextLevel());
+    }
+
+    private IEnumerator NextLevel()
+    {
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+            
+    }
+}
